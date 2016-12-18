4996847a88c090ae327d0090b99550cca8d08eb9
pycpp <tdani87@gmail.com>
2016-12-18 18:42:41 +0100

    hello

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include "mainwindow.h"
+#include <iostream>
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    std::clog << "Hello World!" << std::endl;
     w.show();
 
     return a.exec();
